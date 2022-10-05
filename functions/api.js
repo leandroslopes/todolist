e83967d7eb64e3584c62dd6383bdba62dd881630
Leandro S Lopes <programador.leandrolopes@gmail.com>
2022-10-05 00:58:46 +0000

    Updates and bux fixes show date and hour

--- functions/api.js
+++
@@ -1,6 +0,0 @@
-exports.handler = async () => {
-    return {
-        body: 'test',
-        statusCode: 200
-    }
-}
--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,15 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer" 
-      app
-    >
+    <v-navigation-drawer v-model="drawer" app>
       <v-list-item>
         <v-list-item-content>
-          <v-list-item-title class="text-h6">
-            Todolist
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            Seu app de tarefas
-          </v-list-item-subtitle>
+          <v-list-item-title class="text-h6"> Todolist </v-list-item-title>
+          <v-list-item-subtitle> Seu app de tarefas </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
-
       <v-divider></v-divider>
-
-      <v-list
-        dense
-        nav
-      >
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          :to="item.to"
-          link
-        >
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" :to="item.to" link>
           <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
@@ -37,62 +20,48 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-
-    <v-app-bar
-      app
-      color="primary"
-      dark
-      prominent
-      src="sunrise.jpg"
-    >
+    <v-app-bar app color="primary" dark prominent src="sunrise.jpg">
       <template v-slot:img="{ props }">
         <v-img
           v-bind="props"
           gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
         ></v-img>
       </template>
-
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
-      <v-app-bar-title >
-        
-        Todolist
-        
-        <Clock class="subtitle-2 font-weight-thin pr-100"/> 
-
+      <v-app-bar-title>
+        <span>Todolist</span>
+        <Clock class="subtitle-2 font-weight-thin pr-100" />
       </v-app-bar-title>
-
       <v-spacer></v-spacer>
-
       <v-btn icon>
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
-
     </v-app-bar>
-
     <v-main>
-      
       <router-view></router-view>
-
     </v-main>
-
   </v-app>
-
 </template>
 
 <script>
-import Clock from "./components/Clock.vue"
+import Clock from "./components/Clock.vue";
 
-  export default {
-    components: {
-      Clock
-    },
-    data: () => ({ 
-      drawer: null,
-      items: [
-          { title: 'Todolist', icon: 'mdi-format-list-checks', to: '/' },
-          { title: 'Sobre', icon: 'mdi-help-box', to: '/about' },
-      ],
-    }),
-  }
+export default {
+  components: {
+    Clock,
+  },
+  data: () => ({
+    drawer: null,
+    items: [
+      { title: "Todolist", icon: "mdi-format-list-checks", to: "/" },
+      { title: "Sobre", icon: "mdi-help-box", to: "/about" },
+    ],
+  }),
+};
 </script>
+
+<style>
+.v-app-bar-title__content {
+  width: 100%;
+}
+</style>
